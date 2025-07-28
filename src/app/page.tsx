69a68c3fa2e550f466b14c363104ad118cb9c283
N Lakshanaa IT <nlakshanaa.it2023@citchennai.net>
2025-07-28 05:25:07 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-import { AuthForm } from "@/components/auth-form";
+import dynamic from 'next/dynamic'
 import Logo from "@/components/logo";
+import { Skeleton } from '@/components/ui/skeleton';
+
+const AuthForm = dynamic(() => import('@/components/auth-form').then(mod => mod.AuthForm), {
+  ssr: false,
+  loading: () => <AuthFormSkeleton />,
+});
+
+const AuthFormSkeleton = () => {
+  return (
+    <div className="space-y-6">
+      <div className="flex flex-col space-y-2 text-center">
+        <Skeleton className="h-8 w-48 mx-auto" />
+        <Skeleton className="h-4 w-64 mx-auto" />
+      </div>
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-12" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-12" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <Skeleton className="h-10 w-full" />
+      </div>
+      <Skeleton className="h-4 w-48 mx-auto" />
+    </div>
+  )
+}
+
 
 export default function AuthenticationPage() {
   return (
